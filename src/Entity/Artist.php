bdf3fa5da300ec0e6185a860e1544d0674f8ba43
Mathias Pinon <mathias.pinon@etudiant.univ-reims.fr>
2023-05-09 16:31:47 +0200

    Réalisation de la classe Artist.php

--- src/Entity/Artist.php
+++ src/Entity/Artist.php
@@ -6,5 +6,23 @@ namespace Entity;
 
 class Artist
 {
+    private string $id;
+    private string $name;
+
+    /**
+     * @return string
+     */
+    public function getId(): string
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return string
+     */
+    public function getName(): string
+    {
+        return $this->name;
+    }
 
 }
